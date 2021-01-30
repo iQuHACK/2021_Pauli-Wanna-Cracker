b6d68a0a3d4993e12baec50956e9f55453ffabed
Charles Yuan <charlesyuan314@gmail.com>
2021-01-30 14:43:36 -0500

    QC stuff

--- game.py
+++ game.py
@@ -9,28 +9,35 @@ import scipy
 API_KEY = open("api.env", "r").read().strip()
 provider = IonQProvider(token=API_KEY)
 
+bank_account = []
+ledger = {}
 
-GAMMA = 0
+
+def measure(qubit, basis):
+    coin_flip = random(0.5)
+    if basis == "0/1" and qubit in ('+', '-'):
+        return '0' if coin_flip else '1'
+    elif basis == "+/-" and qubit in ('0', '1'):
+        return '+' if coin_flip else '-'
+    return qubit
 
 
-def u_hat(theta, phi):
-    return np.matrix([[np.exp(1j * phi) * np.cos(theta / 2), np.sin(theta / 2)],
-                      [-np.sin(theta / 2), np.exp(-1j * phi) * np.cos(theta / 2)]])
+class State:
+    def __init__(self, bitstring):
+        self.bitstring = bitstring
+class Bill:
+    def __init__(self, serial_number, state):
+        self.serial_number = serial_number
+        self.state = state
 
-d_hat = u_hat(np.pi, 0)
-j_hat = np.matrix(scipy.linalg.expm(np.kron(-1j * GAMMA * d_hat, d_hat / 2)))
+def verify_with_bank(serial_number, state_qc):
+    pass
 
-q_hat = u_hat(0, np.pi / 2)
+def measure_bit(serial_number, q_i):
+    pass
 
-qc = QuantumCircuit(2)
-qc.unitary(Operator(j_hat), [0, 1], label="j_hat")
-#qc.y(0)
-#qc.x(0)
-#qc.y(1)
-#qc.x(1)
-qc.x(0)
-qc.z(0)
-qc.unitary(Operator(j_hat.H), [0, 1], label="j_hat*")
+qc = QuantumCircuit(1)
+
 print(qc)
 
 #backend = provider.get_backend("ionq_simulator")
